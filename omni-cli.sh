c5225645cee4da41cb29edda3b777b67cfe7cb85
wrought <mattsenate@gmail.com>
2014-09-16 01:00:08 -0700

    adds notes for shell script

---
+++ omni-cli.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+##
+## Goal
+##
+# The point here is to provide a command-line interface to managing systems for the omni oakland collective.
+# To start, the goal is to add users to email lists and generate user accounts on various web systems.
+# This script is intended to be run locally and should only require access to the server(s) in question.
+
+##
+## Pseudocode
+##
+# 1. Ask for:
+#    + username
+#    + password (default random-generated e.g. xkcd style)
+#    + email
+# 2. Create accounts
+#    + WordPress
+#        - use wp-cli or similar https://github.com/wp-cli/wp-cli
+#        - probably wp-cli-ssh is perfect for one-liner https://github.com/x-team/wp-cli-ssh
+#    + MediaWiki
+#        - Modify maintenance/createAndPromote.php script as recommended here https://stackoverflow.com/questions/928594/script-to-add-user-to-mediawiki
+#        - See createUserWithEmail.php in this directory, extends to include and validate email address.
